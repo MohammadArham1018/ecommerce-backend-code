6d19fabbe92a4f0479e71c09aababba870050d28
Arham <arhamansari1018@gmail.com>
2026-02-11 21:03:23 +0530

    some bug fix in db connection

--- database/db.js
+++ database/db.js
@@ -5,16 +5,11 @@ const { Client } = pkg;
 config();
 
 const database = new Client({
-    user : process.env.DB_USER,
-    host : process.env.DB_HOST,
-    database : process.env.DB_NAME,
-    password : process.env.DB_PASSWORD,
-    port : process.env.DB_PORT,
-    ssl: {
-    required: true,
-    rejectUnauthorized: false
-    }
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 try{
     await database.connect();
